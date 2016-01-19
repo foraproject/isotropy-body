a880095201efc812ddbcf245948fc9beac372531
Jeswin Kumar <jeswinpk@agilehead.com>
2016-01-19 20:27:59 +0530

    Initial commit

---
+++ src/isotropy-body.js
@@ -0,0 +1,14 @@
+import coBody from "co-body";
+import co from "co";
+
+/*
+    Currently, this simply wraps co-body without doing anything.
+    We do this because:
+        a) Isotropy projects should only depend on approved libs initially
+        b) We might want to change the underlying implementation later.
+*/
+const asyncBody = async (ctx) => {
+    return await coBody(ctx);
+};
+
+export default asyncBody;
---
+++ src/test/test.js
@@ -0,0 +1,46 @@
+import __polyfill from "babel-polyfill";
+import request from "supertest";
+import parse from "../isotropy-body";
+import koa from "koa";
+import should from "should";
+
+describe('parse(req, opts)', () => {
+    describe('with valid form body', () => {
+        it('should parse', () => {
+            return new Promise((resolve, reject) => {
+                const app = new koa();
+
+                app.use(async (ctx) => {
+                    const body = await parse(ctx);
+                    body.foo.bar.should.equal('baz');
+                    resolve();
+                });
+
+                request(app.listen())
+                    .post('/')
+                    .type('form')
+                    .send({ foo: { bar: 'baz' }})
+                    .end(function(){});
+            });
+        });
+    });
+
+    describe('with valid json', function() {
+        it('should parse', function() {
+            return new Promise((resolve, reject) => {
+                const app = new koa();
+
+                app.use(async (ctx) => {
+                    const body = await parse(ctx);
+                    body.should.eql({ foo: 'bar' });
+                    resolve();
+                });
+
+                request(app.listen())
+                    .post('/')
+                    .send({ foo: 'bar' })
+                    .end(function(){});
+            });
+        });
+    });
+});
