68098ecaf3b938911ea0a69c306dd6fe2bb6a813
Jeswin Kumar <jeswinpk@agilehead.com>
2016-01-25 10:21:54 +0530

    Updated tests

--- src/test/test.js
+++ src/test/test.js
@@ -5,63 +5,58 @@ import koa from "koa";
 import should from "should";
 
 describe('parse(req, opts)', () => {
-  describe('with valid form body', () => {
-    it('should parse form', () => {
-      return new Promise((resolve, reject) => {
-        const app = new koa();
 
-        app.use(async (ctx) => {
-          const body = await parse(ctx);
-          body.foo.bar.should.equal('baz');
-          ctx.body = "Hello world";
-          resolve();
-        });
+  it('should parse form', () => {
+    return new Promise((resolve, reject) => {
+      const app = new koa();
 
-        request(app.listen())
-        .post('/')
-        .type('form')
-        .send({ foo: { bar: 'baz' }})
-        .end(()=>{});
+      app.use(async (ctx) => {
+        const body = await parse(ctx);
+        body.foo.bar.should.equal('baz');
+        ctx.body = "Hello world";
+        resolve();
       });
+
+      request(app.listen())
+      .post('/')
+      .type('form')
+      .send({ foo: { bar: 'baz' }})
+      .end(()=>{});
     });
   });
 
-  describe('with valid json', function() {
-    it('should parse json', function() {
-      return new Promise((resolve, reject) => {
-        const app = new koa();
+  it('should parse json', function() {
+    return new Promise((resolve, reject) => {
+      const app = new koa();
 
-        app.use(async (ctx) => {
-          const body = await parse(ctx);
-          body.should.eql({ foo: 'bar' });
-          resolve();
-        });
-
-        request(app.listen())
-        .post('/')
-        .send({ foo: 'bar' })
-        .end(()=>{});
+      app.use(async (ctx) => {
+        const body = await parse(ctx);
+        body.should.eql({ foo: 'bar' });
+        resolve();
       });
+
+      request(app.listen())
+      .post('/')
+      .send({ foo: 'bar' })
+      .end(()=>{});
     });
   });
 
-  describe('with valid json', function() {
-    it('should parse text', function() {
-      return new Promise((resolve, reject) => {
-        const app = new koa();
+  it('should parse text', function() {
+    return new Promise((resolve, reject) => {
+      const app = new koa();
 
-        app.use(async (ctx) => {
-          const body = await parse(ctx);
-          body.should.eql('hello world');
-          resolve();
-        });
-
-        request(app.listen())
-        .post('/')
-        .set('content-type', 'text/plain')
-        .send('hello world')
-        .end(()=>{});
+      app.use(async (ctx) => {
+        const body = await parse(ctx);
+        body.should.eql('hello world');
+        resolve();
       });
+
+      request(app.listen())
+      .post('/')
+      .set('content-type', 'text/plain')
+      .send('hello world')
+      .end(()=>{});
     });
   });
 
