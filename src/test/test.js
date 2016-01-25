f0628cb385dc4c7d9b75748a0a233b77f6c4ea58
Jeswin Kumar <jeswinpk@agilehead.com>
2016-01-25 10:18:11 +0530

    Added another test. Not that it matters, since we're just wrapping around co-body

--- src/test/test.js
+++ src/test/test.js
@@ -6,7 +6,7 @@ import should from "should";
 
 describe('parse(req, opts)', () => {
   describe('with valid form body', () => {
-    it('should parse', () => {
+    it('should parse form', () => {
       return new Promise((resolve, reject) => {
         const app = new koa();
 
@@ -27,7 +27,7 @@ describe('parse(req, opts)', () => {
   });
 
   describe('with valid json', function() {
-    it('should parse', function() {
+    it('should parse json', function() {
       return new Promise((resolve, reject) => {
         const app = new koa();
 
@@ -44,4 +44,25 @@ describe('parse(req, opts)', () => {
       });
     });
   });
+
+  describe('with valid json', function() {
+    it('should parse text', function() {
+      return new Promise((resolve, reject) => {
+        const app = new koa();
+
+        app.use(async (ctx) => {
+          const body = await parse(ctx);
+          body.should.eql('hello world');
+          resolve();
+        });
+
+        request(app.listen())
+        .post('/')
+        .set('content-type', 'text/plain')
+        .send('hello world')
+        .end(()=>{});
+      });
+    });
+  });
+
 });
